a73514aacb60c9aba090fc6533ff0addbf8098ff
Angelinn117 <lrs19110749@purisima.tecnm.mx>
2023-04-19 19:53:50 -0600

    Agragación de la clase "Transform" optimizado según Chat GTP.

--- OptimizatedCode.py
+++ OptimizatedCode.py
@@ -40,27 +40,21 @@ class Transform:
     def operationDataFrame(self, df_all):
         # Se seleccionan solo las siguientes columnas:
         columns = ["ISIN", "Mnemonic", "Date", "Time", "StartPrice", "EndPrice", "MinPrice", "MaxPrice", "TradedVolume"]
-        df_all = df_all.loc[:, columns]
+        df_all = df_all[columns]
 
-        # Se creará una nueva columna en el df con el precio de apertura.
-        df_all['opening_price'] = df_all.sort_values(by=['Time']).groupby(['ISIN', 'Date'])['StartPrice'].transform(
-            'first')
-
-        # Se creará una nueva columna en el df con el precio de cierre.
-        df_all['closing_price'] = df_all.sort_values(by=['Time']).groupby(['ISIN', 'Date'])['EndPrice'].transform(
-            'last')
-
-        # Se agrupa la informacion del df_all por "ISIN y Date":
-        # Nota, se agregó la columna de "Time", "StartPrice" y "EndPrice".
-        df_all = df_all.groupby(['ISIN', 'Date'],
-                                as_index=False).agg(Time=('Time', 'min'),
-                                                    opening_price_eur=('opening_price', 'min'),
-                                                    closing_price_eur=('closing_price', 'min'),
-                                                    minimum_price_eur=('MinPrice', 'min'),
-                                                    maximum_price_eur=('MaxPrice', 'max'),
-                                                    daily_traded_volume=('TradedVolume', 'sum'),
-                                                    StartPrice_eur=('StartPrice', 'min'),
-                                                    EndPrice_eur=('EndPrice', 'min'))
+        df_all = (df_all
+                  .assign(
+            opening_price=lambda x: x.sort_values(by='Time').groupby(['ISIN', 'Date'])['StartPrice'].transform('first'),
+            closing_price=lambda x: x.sort_values(by='Time').groupby(['ISIN', 'Date'])['EndPrice'].transform('last'))
+                  .groupby(['ISIN', 'Date'], as_index=False)
+                  .agg(Time=('Time', 'min'),
+                       opening_price_eur=('opening_price', 'min'),
+                       closing_price_eur=('closing_price', 'min'),
+                       minimum_price_eur=('MinPrice', 'min'),
+                       maximum_price_eur=('MaxPrice', 'max'),
+                       daily_traded_volume=('TradedVolume', 'sum'),
+                       StartPrice_eur=('StartPrice', 'min'),
+                       EndPrice_eur=('EndPrice', 'min')))
 
         # Convertir la columna 'Date' y 'Time' en tipo datetime y crear DatetimeIndex:
         # Esto significa que cada fila tendrá como índice la fecha y hora a la que corresponde el registro.
